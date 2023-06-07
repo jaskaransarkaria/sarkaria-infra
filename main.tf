8b21679542aea07f79bd018d193a743f82d22123
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2023-06-07 15:40:42 +0100

    feat: 🎸 put cluster in private vpc

--- main.tf
+++ main.tf
@@ -11,20 +11,15 @@ data "civo_size" "small" {
   }
 }
 
-# Create a firewall
-resource "civo_firewall" "firewall" {
-  name = "firewall"
+resource "civo_network" "network" {
+  label  = "private-vpc-${var.CLUSTER_NAME}"
+  region = "LON1"
 }
 
-resource "civo_firewall_rule" "k3s-api-server" {
-  firewall_id = civo_firewall.firewall.id
-  protocol    = "tcp"
-  start_port  = "6443"
-  end_port    = "6443"
-  cidr        = [var.MY_IP_ADDRESS]
-  direction   = "ingress"
-  label       = "kubernetes-api-server"
-  action      = "allow"
+resource "civo_firewall" "firewall" {
+  name                 = "k3s-${var.CLUSTER_NAME}-firewall"
+  create_default_rules = true
+  network_id           = civo_network.network.id
 }
 
 resource "civo_firewall_rule" "http" {
@@ -34,8 +29,9 @@ resource "civo_firewall_rule" "http" {
   end_port    = "80"
   cidr        = ["0.0.0.0/0"]
   direction   = "ingress"
-  label       = "http"
   action      = "allow"
+  label       = "http"
+  depends_on  = [civo_firewall.firewall]
 }
 
 resource "civo_firewall_rule" "https" {
@@ -45,20 +41,32 @@ resource "civo_firewall_rule" "https" {
   end_port    = "443"
   cidr        = ["0.0.0.0/0"]
   direction   = "ingress"
-  label       = "https"
   action      = "allow"
+  label       = "https"
+  depends_on  = [civo_firewall.firewall]
 }
 
-# Create a cluster
-resource "civo_kubernetes_cluster" "cluster" {
-  region = "LON1"
-  name   = var.CLUSTER_NAME
-  # applications = "Nginx,Cert Manager,Metrics Server,Civo cluster autoscaler,Argo CD"
-  applications = "Nginx,metrics-server,argo-cd,civo-cluster-autoscaler,cert-manager"
-  # applications = "Nginx,Metrics Server"
+resource "civo_firewall_rule" "k3s_api_server" {
   firewall_id = civo_firewall.firewall.id
+  protocol    = "tcp"
+  start_port  = "6443"
+  end_port    = "6443"
+  cidr        = [var.MY_IP_ADDRESS]
+  direction   = "ingress"
+  action      = "allow"
+  label       = "k3s-api-server"
+  depends_on  = [civo_firewall.firewall]
+}
+
+resource "civo_kubernetes_cluster" "cluster" {
+  region       = "LON1"
+  name         = var.CLUSTER_NAME
+  applications = "Nginx,metrics-server,argo-cd,civo-cluster-autoscaler,cert-manager"
+  firewall_id  = civo_firewall.firewall.id
+  network_id   = civo_network.network.id
+
   pools {
-    size       = element(data.civo_size.small.sizes, 0).name
+    size       = element(data.civo_size.small.sizes, 1).name
     node_count = 3
   }
 }
